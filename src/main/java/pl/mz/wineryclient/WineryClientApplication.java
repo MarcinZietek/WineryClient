3e367e00b03a66a4e9683f47863de78f12eb6470
MarcinZietek <marcin.it.zietek@gmail.com>
2022-07-25 14:46:20 +0200

    initial commit #1

---
+++ src/main/java/pl/mz/wineryclient/WineryClientApplication.java
@@ -0,0 +1,13 @@
+package pl.mz.wineryclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WineryClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WineryClientApplication.class, args);
+    }
+
+}
