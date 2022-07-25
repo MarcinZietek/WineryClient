f2ce2c1a573c498d9b58f1b3165f70bc856cab73
MarcinZietek <marcin.it.zietek@gmail.com>
2022-07-25 15:15:13 +0200

    added WineryClient #3

--- src/main/java/pl/mz/wineryclient/client/CustomerClient.java
+++ src/main/java/pl/mz/wineryclient/client/CustomerClient.java
@@ -4,6 +4,10 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
+import pl.mz.wineryclient.model.CustomerDto;
+
+import java.net.URI;
+import java.util.UUID;
 
 @Component
 @ConfigurationProperties(prefix = "api.winery", ignoreUnknownFields = false)
@@ -15,5 +19,26 @@ public class CustomerClient {
 
     public CustomerClient(RestTemplateBuilder restTemplateBuilder) {
         this.restTemplate = restTemplateBuilder.build();
+    }
+
+    public CustomerDto getCustomerById(UUID customerId){
+        return restTemplate.getForObject(apihost + CUSTOMER_PATH_V1
+                + customerId.toString(), CustomerDto.class);
+    }
+
+    public URI saveNewCustomer(CustomerDto customerDto){
+        return restTemplate.postForLocation(apihost +
+                CUSTOMER_PATH_V1, customerDto); }
+
+    public void updateCustomer(UUID uuid, CustomerDto customerDto){
+        restTemplate.put(apihost + CUSTOMER_PATH_V1 + uuid.toString(), customerDto);
+    }
+
+    public void deleteCustomer(UUID uuid){
+        restTemplate.delete(apihost + CUSTOMER_PATH_V1 + uuid);
+    }
+
+    public void setApihost(String apihost) {
+        this.apihost = apihost;
     }
 }
